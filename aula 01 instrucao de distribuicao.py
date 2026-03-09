5c22aee8cabdae289312690e3278be6c74191a7c
ISC0669 <isaac.almeida.ribeiro@escola.pr.gov.br>
2026-03-09 10:23:46 -0300

    Add files via upload

---
+++ aula 01 instrucao de distribuicao.py
@@ -0,0 +1,7 @@
+# Variável é um espaço de memória que tem um nome e guarda algum dado do meu programa
+nome = 'Isaac'
+sobrenome = 'Almeida'
+numero = 1
+resultado = numero + numero + 5
+
+print(resultado)
