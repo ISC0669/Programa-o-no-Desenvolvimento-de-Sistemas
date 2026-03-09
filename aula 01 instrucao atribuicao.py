c4ffa71b605b4279951a80b685a0e863050cd336
ISC0669 <isaac.almeida.ribeiro@escola.pr.gov.br>
2026-03-09 11:15:54 -0300

    Add files via upload

--- aula 01 instrucao atribuicao.py
+++ aula 01 instrucao atribuicao.py
@@ -4,5 +4,13 @@ sobrenome = 'Almeida'
 numero = 1
 resultado = numero + numero + 5
 
+print(resultado)
+
+#variavel e um espaço de memoria que tem um nome e guarda algum dado do meu programa
+numero=1
+
+# Expressoes são qualquer combinações de valor, variável e operadores
+resultado=numero + numero + 5
+resultado = (numero * (numero*numero) +33)
 
 print(resultado)
